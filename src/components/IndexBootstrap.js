9139108e80c5fa0401f874368cef0294a86a9d1f
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-21 07:34:53 -0700

    working on the FE

---
+++ src/components/IndexBootstrap.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Jumbotron from "./Jumbo";
+import Navbar from "./navbar";
+import Card from "./card";
+
+function IndexBootstrap() {
+  return (
+    <>
+      <Jumbotron />
+      <Navbar />
+      <Card />
+    </>
+  );
+}
+
+export default IndexBootstrap;
---
+++ src/components/card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <div className="card mb-4">
+        <div className="card-header">
+          <h3>Card title</h3>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Card content</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
