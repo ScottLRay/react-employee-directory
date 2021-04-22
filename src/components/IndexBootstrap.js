b1398cba812d754251d590d3ae727cac1f1bac92
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-22 09:14:36 -0700

    working on getting things to show

--- src/components/IndexBootstrap.js
+++ src/components/IndexBootstrap.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Jumbotron from "./Jumbo";
 import Navbar from "./navbar";
-import Card from "./card";
+import Card from "./Card";
 
 function IndexBootstrap() {
   return (
--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-function Card() {
+function Card({ employees }) {
   return (
-    <div>
-      <div className="card mb-4">
-        <div className="card-header">
-          <h3>employee name</h3>
-        </div>
-        <div className="card-body">
-          <p className="card-text">employee postions</p>
-        </div>
+    <div className="card" style={{ width: "18rem" }}>
+      <img
+        src={employees.name.first[0]}
+        className="card-img-top"
+        alt={employees.picture.thumbnail[0]}
+      />
+      <div className="card-body">
+        <ul>
+          <li>
+            <strong>Name:</strong> {employees.name.first[0]}
+          </li>
+          <li>
+            <strong>Phone</strong> {employees.phone[0]}
+          </li>
+          <li>
+            <strong>Email</strong> {employees.email[0]}
+          </li>
+          <li>
+            <strong>DOB</strong> {employees.dob[0]}
+          </li>
+        </ul>
       </div>
     </div>
   );
 }
-
 export default Card;
